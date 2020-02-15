3d9a634c9e3bbe9616ee0200ebbaa8e8ddc43cc1
mikareich <mikareich0@gmail.com>
2020-02-15 16:19:14 +0100

    update modal

--- script.js
+++ script.js
@@ -1,76 +1,63 @@
 /* eslint-disable no-use-before-define */
 let player = "a";
-let clicked = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let points = { a: 0, b: 0 };
 const tttAreaDIV = document.querySelector(".ttt-area");
 const cellsDIV = document.querySelectorAll(".ttt-area div");
 const modalDIV = document.querySelector(".modal");
-function controlLines(cell, cellIndex) {
+modalDIV.querySelector(".reset").addEventListener("click", render)
+function controlLines(cellIndex) {
   /* get vertical and horizontal rows in which the cell is 
   located by adding the first cell of each row "min", the 
   second cell min + 1 and the third cell min + 2 to the row 
   array 
   cellIndex % 3 = column of the cell*/
+  const lines = [];
   // vertical row
-  const minV = cellIndex % 3
-  const verticalLine = [clicked[minV], clicked[minV + 3], clicked[minV + 6]];
-  
+  const minV = cellIndex % 3;
+  const verticalLine = [minV, minV + 3, minV + 6];
+  lines.push(verticalLine);
   // horizontal row
-  const minH = cellIndex - (cellIndex % 3)
-  const horizontalLine = [clicked[minH], clicked[minH+1], clicked[minh=2]]
-  const sumh = [
-    clicked[0] + clicked[1] + clicked[2],
-    clicked[3] + clicked[4] + clicked[5],
-    clicked[6] + clicked[7] + clicked[8]
-  ];
-  const sumv = [
-    clicked[0] + clicked[3] + clicked[6],
-    clicked[1] + clicked[4] + clicked[7],
-    clicked[2] + clicked[5] + clicked[8]
-  ];
-  const sumd = [
-    clicked[0] + clicked[4] + clicked[8],
-    clicked[2] + clicked[4] + clicked[6]
-  ];
-
-  const max = [Math.max(...sumh), Math.max(...sumv), Math.max(...sumd)];
-  const min = [Math.min(...sumh), Math.min(...sumv), Math.min(...sumd)];
-
-  if (max.includes(3) || min.includes(-3)) {
-    if (max.includes(3)) {
-      modalDIV.querySelector("#winner").textContent = "RED WINS!";
-    }
-    if (min.includes(-3)) {
-      modalDIV.querySelector("#winner").textContent = "BLUE WINS!";
-    }
-    // display modal
-    modalDIV.classList.remove("hidden");
-    modalDIV.querySelector("#reset").addEventListener("click", render);
-  }
+  const minH = cellIndex - (cellIndex % 3);
+  const horizontalLine = [minH, minH + 1, minH + 2];
+  lines.push(horizontalLine);
+  // diagonal row
+  if (cellIndex === 0 || cellIndex === 8) lines.push([0, 4, 8]);
+  else if (cellIndex === 2 || cellIndex === 6) lines.push([2, 4, 6]);
+  else if (cellIndex === 4) lines.push([0, 4, 8], [2, 4, 6]);
+  // control lines
+  return lines.some(row =>
+    // checks that every cell in the row contains the same class
+    row.every(index => cellsDIV[index].classList.contains(player))
+  )
+    ? "point"
+    : lines.every(row =>
+        row.every(index => cellsDIV[index].classList.length === 1)
+      )
+    ? "draw"
+    : false;
 }
 
 function render() {
   modalDIV.classList.add("hidden");
-  clicked = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   // remove classes
   cellsDIV.forEach(cell => {
-    cell.classList.remove("clickedA");
-    cell.classList.remove("clickedB");
+    cell.classList.remove("a");
+    cell.classList.remove("b");
   });
 }
 
-function setSymbol(e, index) {
-  const cell = e.target;
-  if (cell.className === "clickedA" || cell.className === "clickedB") return;
-  cell.className = `clicked${player.toUpperCase()}`;
-  if (player === "a") {
-    clicked[index] = 1;
-  } else {
-    clicked[index] = -1;
-  }
-  controlLines(cell, index);
-  player = player === "a" ? "b" : "a";
-}
-
-cellsDIV.forEach((cell, i) =>
-  cell.addEventListener("click", e => setSymbol(e, i))
+cellsDIV.forEach((cell, index) =>
+  cell.addEventListener("click", function() {
+    if (this.className === "a" || this.className === "b") return;
+    this.className = player;
+    let status = controlLines(index);
+    if (status) {
+      modalDIV.classList.remove("hidden");
+      modalDIV.querySelector(".winner").textContent =
+        status === "draw" ? "DRAW" : player === "a" ? "RED WINS" : "BLUE WINS";
+      if(status === "point") points[player]++
+      modalDIV.querySelector(".points").textContent = `${points["a"]}:${points["b"]}`
+    }
+    player = player === "a" ? "b" : "a";
+  })
 );
